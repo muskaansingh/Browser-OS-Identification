ec0e5588e1233ae686fab79045e2502754b854b8
Muskaan Singh <muskaansingh235@gmail.com>
2020-08-06 14:40:36 +0530

    Updates

---
+++ precache-manifest.26d184b27d2cab3c51b9ca8dbdc04cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888087a809467696d0c5e5dfaeb82ebc",
+    "url": "/Browser-OS-Identification/index.html"
+  },
+  {
+    "revision": "664ed2f467216904dd0d",
+    "url": "/Browser-OS-Identification/static/js/2.26a8a2a4.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/Browser-OS-Identification/static/js/2.26a8a2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f819a8536f09ffc236a",
+    "url": "/Browser-OS-Identification/static/js/main.65e32e13.chunk.js"
+  },
+  {
+    "revision": "02f3210a313d11e2261d",
+    "url": "/Browser-OS-Identification/static/js/runtime-main.de67f4c7.js"
+  }
+]);
